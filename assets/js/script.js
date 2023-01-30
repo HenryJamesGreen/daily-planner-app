c9180febd80ca04bacd769f16c4d7a8e43404e6f
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-01-30 20:22:29 +0000

    added localstorage retrieval) git push

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,13 @@ function displayHours() {
       $(this).removeClass("future");
       $(this).removeClass("present");
     } else if (hourSlot === CurrentTime) {
-      $(this).removeClass("past");
       $(this).addClass("present");
+      $(this).removeClass("past");
       $(this).removeClass("future");
     } else {
+      $(this).addClass("future");
       $(this).removeClass("present");
       $(this).removeClass("past");
-      $(this).addClass("future");
     }
   });
 }
@@ -33,7 +33,22 @@ $(".saveBtn").on("click", function () {
   let eventHour = $(this).parent().attr("id");
   let eventText = $(this).siblings(".description").val();
 
+  localStorage.setItem(eventHour, eventText);
+
   
-  localStorage.setItem("todaysTasks", JSON.stringify(eventText));
-  $("#todaysTasks").text(eventText);
 });
+
+function getItem() {
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+}
+
+getItem()
