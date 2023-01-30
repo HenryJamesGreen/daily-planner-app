77de31fdff9ee2b4e75c63cc46a72f9b6325db9c
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-01-30 14:23:42 +0000

    added if statement for colour backgrounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
 //current date.
-var time = moment().format("DD MMMM YYYY");
-$("#currentDay").text(time);
+const date = moment().format("DD MMMM YYYY");
+$("#currentDay").text(date);
 
+//current time check
+const CurrentTime = moment().hour();
+console.log(CurrentTime);
+
+function displayHours() {
+  // run through time-blocks, grab the id and set it to a number value to run through the if statemnet.
+  $(".time-block").each(function () {
+    var hourSlot = parseInt($(this).attr("id"));
+
+    //add classes to change BG properties. REMEMBER - also remove classes, to prevent confusion!
+    if (hourSlot < CurrentTime) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    } else if (hourSlot === CurrentTime) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+displayHours();
